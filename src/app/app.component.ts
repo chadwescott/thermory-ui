15a1f27e82840e900259aa55edc0c919f1af0278
Chad Wescott <vndTeksysCXW@wegmans.com>
2018-03-03 18:42:01 -0500

    Wired up menu links and added icons.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,32 +9,4 @@ import { Router } from '@angular/router';
 export class AppComponent {
   constructor(private router: Router) {
   }
-
-  showAudit() {
-    this.router.navigate(['audit'])
-  }
-  
-  showCatalog() {
-    this.router.navigate(['catalog']);
-  }
-
-  showHome() {
-    this.router.navigate([''])
-  }
-
-  showInventory() {
-    this.router.navigate(['inventory'])
-  }
-
-  showPurchaseOrderList() {
-    this.router.navigate(['incoming'])
-  }
-
-  showSalesOrderList() {
-    this.router.navigate(['sales-orders'])
-  }
-
-  showUserList() {
-    this.router.navigate(['users'])
-  }
 }
--- src/app/components/page-header/page-header.component.ts
+++ src/app/components/page-header/page-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-header',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  showAudit() {
+    this.router.navigate(['audit'])
+  }
+  
+  showCatalog() {
+    this.router.navigate(['catalog']);
+  }
+
+  showHome() {
+    this.router.navigate([''])
+  }
+
+  showInventory() {
+    this.router.navigate(['inventory'])
+  }
+
+  showPurchaseOrderList() {
+    this.router.navigate(['incoming'])
+  }
+
+  showSalesOrderList() {
+    this.router.navigate(['sales-orders'])
+  }
+
+  showUserList() {
+    this.router.navigate(['users'])
+  }
 }
